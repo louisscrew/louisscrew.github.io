2eb7affde88fca8dd287bb9d7f958f33538dca89
CORP\quawen <quan.wen@capgemini.com>
2021-04-02 13:21:58 +0800

    1111

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var app = new Vue({
     data: {
         scanner: null,
         activeCameraId: null,
+        activeCamera: null,
         cameras: [],
         scans: [],
     },
@@ -11,23 +12,31 @@ var app = new Vue({
         self.scanner = new Instascan.Scanner({
             video: document.getElementById('preview'),
             scanPeriod: 5,
-            mirror:false,//不用镜像
+            mirror:false,//不用镜像模式，镜像模式类似于镜子
         });
         self.scanner.addListener('scan', function (content, image) {
             self.scans.unshift({ date: +Date.now(), content: content });
         });
         Instascan.Camera.getCameras()
             .then(function (cameras) {
-                self.cameras = cameras;
-                for (let c of cameras) {
-                    alert(c.name + 1);
-                }
-                if (cameras.length > 0) {
-                    self.activeCameraId = cameras[0].id;
-                    self.scanner.start(cameras[0]);
+                self.cameras = cameras
+                console.log(cameras)
+                if (cameras.length > 1) {//取后置摄像头
+                    alert("多个摄像头")
+                    let tmpCamera = self.cameras.find((ca) => {
+                        alert("摄像头 type:"+ca.type)
+                        return ca.type === 'environment'
+                    })
+                    if (tmpCamera) {
+                        self.activeCamera = tmpCamera
+                    }
+                } else if (cameras.length === 1) {//一个摄像头
+                    alert("只有一个摄像头")
+                    self.activeCamera = cameras[0]
                 } else {
-                    console.error('No cameras found.');
+                    alert("meiyou shexiangtou ")
                 }
+                self.scanner.start(self.activeCamera)
             })
             .catch(function (e) {
                 console.error(e);
@@ -39,7 +48,7 @@ var app = new Vue({
         },
         selectCamera: function (camera) {
             this.activeCameraId = camera.id;
-            alert(222)
+            // alert(222)
             this.scanner.start(camera);
         },
     },
